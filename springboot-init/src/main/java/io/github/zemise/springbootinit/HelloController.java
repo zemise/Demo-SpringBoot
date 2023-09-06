223b8a604b836e6624985041dba01515b318e6fb
zemise <373933306@qq.com>
2023-09-06 12:03:50 +0800

    learn @Value

--- springboot-init/src/main/java/io/github/zemise/springbootinit/HelloController.java
+++ springboot-init/src/main/java/io/github/zemise/springbootinit/HelloController.java
@@ -1,5 +1,8 @@
 package io.github.zemise.springbootinit;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +16,48 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloController {
+    @Value("${name}")
+    private String name;
+
+    @Value("${person.name}")
+    private String personName;
+
+    @Value("${person.age}")
+    private int personAge;
+
+    @Value("${address[0]}")
+    private String address1;
+
+    @Value("${address[1]}")
+    private String address2;
+
+    @Value("${msg1}")
+    private String msg1;
+
+    @Value("${msg2}")
+    private String msg2;
+
+    @Autowired
+    private Environment env;
+
+    @RequestMapping("/hello2")
+    public String hello2() {
+        System.out.println(name);
+        System.out.println(personName);
+        System.out.println(personAge);
+
+
+        System.out.println(address1);
+        System.out.println(address2);
+
+        System.out.println(msg1);
+        System.out.println(msg2);
+
+        return "hello springboot";
+    }
+
     @RequestMapping("/hello222")
-    public static String hello(){
+    public static String hello() {
         return "Hello spring boot 222!";
     }
 }
