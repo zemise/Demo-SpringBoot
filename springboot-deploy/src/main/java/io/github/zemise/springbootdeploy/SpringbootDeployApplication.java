a4c969de457055705b40fe0baa541e52fbaffef4
zemise <373933306@qq.com>
2023-10-09 10:25:22 +0800

    learn more about depoly

--- springboot-deploy/src/main/java/io/github/zemise/springbootdeploy/SpringbootDeployApplication.java
+++ springboot-deploy/src/main/java/io/github/zemise/springbootdeploy/SpringbootDeployApplication.java
@@ -2,20 +2,17 @@ package io.github.zemise.springbootdeploy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootDeployApplication extends SpringBootServletInitializer {
+//public class SpringbootDeployApplication extends SpringBootServletInitializer {
+public class SpringbootDeployApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootDeployApplication.class, args);
     }
 
-
-    // war包方式
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(SpringbootDeployApplication.class);
-    }
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        return builder.sources(SpringbootDeployApplication.class);
+//    }
 }
