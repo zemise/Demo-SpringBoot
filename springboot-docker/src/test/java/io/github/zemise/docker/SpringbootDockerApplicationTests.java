c627e3a8956fc8bebbcaf99abad87042f83c6466
zemise <373933306@qq.com>
2023-10-09 12:12:53 +0800

    learn docker deploy

---
+++ springboot-docker/src/test/java/io/github/zemise/docker/SpringbootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.zemise.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
